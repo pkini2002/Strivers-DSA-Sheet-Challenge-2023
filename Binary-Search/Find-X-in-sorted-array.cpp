4863591312150bd73bf061d9617aab4ff75eb075
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-06-08 20:39:34 +0530

    Create Find-X-in-sorted-array.cpp

---
+++ Binary-Search/Find-X-in-sorted-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int low=0;
+        int high=n-1;
+        
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==k){
+                return mid;
+            }
+            else if(arr[mid]<k){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+};
