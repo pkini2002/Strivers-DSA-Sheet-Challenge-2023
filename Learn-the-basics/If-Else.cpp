0777c709effdd87b02656ecc085b0fabd4b5c7f7
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-06-11 14:03:05 +0530

    Create If-Else.cpp

---
+++ Learn-the-basics/If-Else.cpp
@@ -0,0 +1,15 @@
+//User function Template for Java
+class Solution{
+    static String compareNM(int n,int m){
+        // code here
+        if(n<m){
+            return "lesser";
+        }
+        else if(n==m){
+            return "equal";
+        }
+        else{
+            return "greater";
+        }
+    }
+}
