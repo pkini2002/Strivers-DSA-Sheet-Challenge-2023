b69be63f74096a4f1c53f20cdc116cf91a71a5cc
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-06-15 22:14:52 +0530

    Create Second-Largest.cpp

---
+++ Arrays/Second-Largest.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    // code here
+	    sort(arr,arr+n);
+	    for(int i=n-1;i>=0;i--){
+	        if(arr[i]!=arr[n-1]){
+	            return arr[i];
+	        }
+	    }
+	    return -1;
+	}
+};
