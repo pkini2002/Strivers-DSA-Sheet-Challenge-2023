619dc0eae701d4dcc0a663c2495a386803417d0f
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-06-16 11:24:54 +0530

    Update Selection-Sort.cpp

--- Sorting/Selection-Sort.cpp
+++ Sorting/Selection-Sort.cpp
@@ -20,3 +20,54 @@ class Solution
        }
     }
 };
+
+// Full Code
+
+#include<bits/stdc++.h>
+
+using namespace std; 
+
+void selectionsort(int a[],int n){
+	int min;
+	int temp;
+	for(int i=0;i<n-1;i++){
+		min=i;
+		for(int j=i+1;j<n;j++){
+			if(a[j]<a[min]){
+				temp=a[j];
+				a[j]=a[min];
+				a[min]=temp;
+			}
+		}
+		temp=a[i];
+		a[i]=a[min];
+		a[min]=temp;
+	}
+
+	cout<<"Elements after sorting is: "<<endl;
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<" ";
+	}
+}
+
+
+int main(){
+	#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+    #endif
+
+   int n;
+   cin>>n;
+
+   int a[n];
+
+   for(int i=0;i<n;i++){
+   	cin>>a[i];
+   }
+    selectionsort(a,n);
+
+	return 0;
+}
