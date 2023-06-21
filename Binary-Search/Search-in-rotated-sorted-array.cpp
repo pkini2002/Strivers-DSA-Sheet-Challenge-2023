214c97cef4a45dff8139caa13046dbd4d26c1e43
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-06-21 18:23:35 +0530

    Update Search-in-rotated-sorted-array.cpp

--- Binary-Search/Search-in-rotated-sorted-array.cpp
+++ Binary-Search/Search-in-rotated-sorted-array.cpp
@@ -30,3 +30,39 @@ bool searchInARotatedSortedArrayII(vector<int>&A, int key) {
     }
     return false;
 }
+
+
+// GFG
+
+bool searchInARotatedSortedArrayII(vector<int>&A, int key) {
+    // Write your code here.
+    int low=0;
+    int high=A.size()-1;
+
+    while(low<=high){
+        int mid=(low+high)/2;
+
+        if(A[mid]==key){
+            return true;
+        }
+
+        // Left sorted
+        if(A[low]<=A[mid]){
+            if(A[low]<=key && key<=A[mid]){
+                 high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        else{
+            if(A[mid]<=key && key<=A[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+    }
+    return false;
+}
