f1795509dd7c95c4ad5e5cd91224f5f24f072cef
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-06-28 10:08:44 +0530

    Create Reverse-Integer.cpp

---
+++ Learn-the-basics/Basic-Math/Reverse-Integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int ans=0;
+    
+        while(x!=0){
+            long long int rem=x%10;
+            ans=ans*10+rem;
+            x/=10;
+        }
+        if(ans>INT_MAX || ans<INT_MIN){
+            return 0;
+        }
+        return ans;
+    }
+};
