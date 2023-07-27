5e3a5d04261e4b8a3f007dd781e211de7d60ccb0
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-07-27 15:56:39 +0530

    Create Intro-Dp.cpp

---
+++ Dynamic-Programming/Intro-Dp.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int prev1=1;
+    int prev2=0;
+    if(n<=1){
+            cout<<n<<endl;
+    }
+    else{
+            int current=0;
+            for(int i=2;i<=n;i++){
+                    current=prev1+prev2;
+                    prev2=prev1;
+                    prev1=current;
+            }
+            cout<<current<<endl;
+    }
+    return 0;
+}
