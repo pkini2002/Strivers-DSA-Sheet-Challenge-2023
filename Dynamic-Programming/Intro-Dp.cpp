0e9a7edab633a30e003766a74560f3289e912202
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-12-07 23:33:22 +0530

    Update Intro-Dp.cpp

--- Dynamic-Programming/Intro-Dp.cpp
+++ Dynamic-Programming/Intro-Dp.cpp
@@ -21,3 +21,32 @@ int main()
     }
     return 0;
 }
+
+// Alternate Solution
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int f(int n,vector<int>&dp){
+	if(n<=1){
+		return n;
+	}
+	if(dp[n]!=-1)
+		return dp[n];
+	return dp[n]=f(n-1,dp)+f(n-2,dp);
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+	// for getting input from input.txt
+	freopen("input.txt","r",stdin);
+	// for writing to output.txt
+	freopen("output.txt","w",stdout);
+#endif
+	int n;
+	cin>>n;
+	vector<int>dp(n+1,-1);
+	cout<<f(n,dp);
+	return 0;
+}
