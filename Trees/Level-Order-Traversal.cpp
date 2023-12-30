a299af1ae974aaee1e7210c952938db5f8f046e6
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-12-30 12:15:52 +0530

    Update Level-Order-Traversal.cpp

--- Trees/Level-Order-Traversal.cpp
+++ Trees/Level-Order-Traversal.cpp
@@ -1,28 +1,33 @@
-vector<int> levelOrder(TreeNode<int> * root){
-    // Write your code here.
-    vector<int>res;
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>res;
 
-    if(root==NULL){
+        if(root==NULL){
+            return res;
+        }
+
+        queue<TreeNode*>nodesQueue;
+        nodesQueue.push(root);
+
+        while(!nodesQueue.empty()){
+            int size=nodesQueue.size();
+            vector<int>row;
+
+            for(int i=0;i<size;i++){
+                TreeNode* node=nodesQueue.front();
+                row.push_back(node->val);
+                nodesQueue.pop();
+
+                if(node->left){
+                    nodesQueue.push(node->left);
+                }
+                if(node->right){
+                    nodesQueue.push(node->right);
+                }
+            }
+            res.push_back(row);
+        }
         return res;
     }
-
-    queue<TreeNode<int>*>nodesQueue;
-
-    nodesQueue.push(root);
-
-    while(!nodesQueue.empty()){
-        int size=nodesQueue.size();
-        for(int i=0;i<size;i++){
-            TreeNode<int>* node=nodesQueue.front();
-            res.push_back(node->data);
-            nodesQueue.pop();
-            if(node->left){
-                nodesQueue.push(node->left);
-            }
-            if(node->right){
-                nodesQueue.push(node->right);
-            }
-        }
-    }
-    return res;
-}
+};
