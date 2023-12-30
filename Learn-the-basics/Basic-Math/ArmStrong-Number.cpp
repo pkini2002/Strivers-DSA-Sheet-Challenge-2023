e7ae03182f5ec46bfe98cdb4385d8cb3dfb0a7d3
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-12-30 12:53:20 +0530

    Update ArmStrong-Number.cpp

--- Learn-the-basics/Basic-Math/ArmStrong-Number.cpp
+++ Learn-the-basics/Basic-Math/ArmStrong-Number.cpp
@@ -20,3 +20,37 @@ class Solution {
         }
     }
 };
+
+// Coding Ninjas ArmStrong Number Solution
+
+int CountDigits(int n){
+	int c=0;
+	while(n!=0){
+		int rem=n%10;
+		c++;
+		n/=10;
+	}
+	return c;
+}
+
+long long int ArmStrongCheck(int n,int count){
+	long long int ans=0;
+	while(n!=0){
+		int rem=n%10;
+		ans+=(pow(rem,count));
+		n/=10;
+	}
+	return ans;
+}
+
+bool checkArmstrong(int n){
+	//Write your code here
+	int count=CountDigits(n);
+	long long int findVal=ArmStrongCheck(n,count);
+
+	if(findVal==n){
+		return true;
+	}
+	return false;
+}
+
